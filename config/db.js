57cd378584aa1727d23e73b5c27537efde745847
Gokul <your-email@example.com>
2026-01-26 15:17:48 +0530

    new changes

--- config/db.js
+++ config/db.js
@@ -1,26 +1,18 @@
-
-
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  if (!process.env.MONGO_URI) {
-    console.error("MONGO_URI is missing");
+  if (!process.env.MONGODB_URI) {
+    console.error(" MONGODB_URI missing");
     process.exit(1);
   }
-console.log("ENV CHECK => MONGO_URI:", process.env.MONGO_URI);
 
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("MongoDB Connection Error:", error.message);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log(" MongoDB Connected");
+  } catch (err) {
+    console.error(" MongoDB Connection Error:", err);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
-
