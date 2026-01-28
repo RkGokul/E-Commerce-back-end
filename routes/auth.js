172a70cae02c922ef751860b9fa11b239f58c445
Gokul <your-email@example.com>
2026-01-28 14:09:47 +0530

    new changes

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const { protect } = require('../middleware/auth');
+const { protect, admin } = require('../middleware/auth');
 
 // Generate JWT Token
 const generateToken = (id) => {
